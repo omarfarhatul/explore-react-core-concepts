7ee7b5e67e2b832c7ba725bd07090a3dd960b7db
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-10-15 19:14:20 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ function App() {
       <h1>Vite + React</h1>
       <Person></Person> 
       <Person></Person> 
-      <Person></Person> 
+      <Person></Person>
+      <Student></Student> 
     </>
   )
 }
@@ -17,8 +18,18 @@ function App() {
 function Person(){
   const age=25;
   const add_age=3;
-  const person={name: 'Sakib', dept: 'Dept. of CSE', batch: 61}
-  return <h3>I am a {person} with age is {age+add_age}</h3>
+  const person={name: 'Sakib', dept: 'Dept. of CSE', batch: 61};
+  return <h3>I am a {person.name} {person.dept} {person.batch} and age is {age+add_age}</h3>
+}
+
+function Student(){
+  return (
+    <div className='student'>
+      <h3>This is a student</h3>
+      <p>Name: Omar Farhatul</p>
+      <p>Age: 30</p>
+    </div>
+  )
 }
 
 export default App
